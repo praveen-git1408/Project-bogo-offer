d62371d457cf19ee28cf4677eeba9eb9465998d2
praveen-git1408 <praveenkalaiselvan630@gmail.com>
2025-09-14 00:34:53 +0530

    Update main.js

--- New Project/assets/js/main.js
+++ New Project/assets/js/main.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".option").forEach(option => {
     option.addEventListener("click", () => {
-      document.querySelectorAll(".option").forEach(o => {
-        o.classList.remove("active");
-        o.querySelector("input[type=radio]").checked = false;
-        const childOptions = o.querySelector(".unit-options");
+      document.querySelectorAll(".option").forEach(element => {
+        element.classList.remove("active");
+        element.querySelector("input[type=radio]").checked = false;
+        const childOptions = element.querySelector(".unit-options");
         if (childOptions) {
           childOptions.classList.remove("open");
         }
       });
-
       option.classList.add("active");
       option.querySelector("input[type=radio]").checked = true;
 
       const unitOptions = option.querySelector(".unit-options");
       if (unitOptions) {
         unitOptions.classList.add("open");
+       document.querySelector(".total-value").textContent = option.querySelector(".new-price").getAttribute("price");
       }
     });
   });
